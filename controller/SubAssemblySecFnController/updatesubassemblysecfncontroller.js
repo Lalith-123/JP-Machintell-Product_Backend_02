c8663aa60d459c5d7b6eed6070fa43677c113ff2
JayaPrakash1906 <jayaprakashtrk8@gmail.com>
2024-04-09 13:09:01 +0530

    .

--- controller/SubAssemblySecFnController/updatesubassemblysecfncontroller.js
+++ controller/SubAssemblySecFnController/updatesubassemblysecfncontroller.js
@@ -3,14 +3,14 @@ const updatesubassemblysecfnController = async(req, res) => {
     try
     {
         const {id} = req.params;
-        const {product_id, sub_secondary_functions_details}= req.body
-        const result = await updatesubassemblysecfnModel(product_id, sub_secondary_functions_details, id);
+        const { sub_secondary_functions_details}= req.body
+        const result = await updatesubassemblysecfnModel( sub_secondary_functions_details, id);
         res.status(200).json({message: 'sub_SecondaryFn Updated Successfully', data: result});
     }
     catch (err)
     {
         console.log(err);
-        res.json({error: 'Not Properly Updated. Pleas try again'});
+        res.json({error: 'Not Properly Updated. Please try again'});
     }
 }
 module.exports = updatesubassemblysecfnController;
--- model/SubAssemblySecFnModel/updatesubassemblysecfnmodel.js
+++ model/SubAssemblySecFnModel/updatesubassemblysecfnmodel.js
@@ -1,7 +1,7 @@
 const con = require('../db');
-const updatesubassemblysecfnModel = (product_id, sub_secondary_functions_details) => {
+const updatesubassemblysecfnModel = ( sub_secondary_functions_details, id) => {
         return new Promise((resolve, reject) => {
-            con.query('UPDATE sub_Sec_functions SET product_id = ?, sub_secondary_functions_details = ?', [product_id, sub_secondary_functions_details],
+            con.query('UPDATE sub_Sec_functions SET sub_secondary_functions_details = ? where product_id = ?', [ sub_secondary_functions_details, id],
             (error, result) => 
             {
                 if(error)
